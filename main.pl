adf88bf3586f08af949f9febacf952f8f3391c95
YatongXing <121362286+YatongXing@users.noreply.github.com>
2025-11-11 18:41:31 -0800

    Update main.pl

--- main.pl
+++ main.pl
@@ -1,30 +1,30 @@
-% 1) my_length
+% 1) my_length/2
 my_length([], 0).
 my_length([_|T], R) :-
   my_length(T, R1),
   R is R1 + 1.
 
-% 2) my_member
+% 2) my_member/2
 my_member(X, [X|_]).
 my_member(X, [_|T]) :- my_member(X, T).
 
-% 3) my_append
+% 3) my_append/3
 my_append([], L2, L2).
 my_append([H|T], L2, [H|R]) :- my_append(T, L2, R).
 
-% 4) my_reverse
+% 4) my_reverse/2
 my_reverse(L, R) :- my_rev_acc(L, [], R).
 my_rev_acc([], Acc, Acc).
 my_rev_acc([H|T], Acc, R) :- my_rev_acc(T, [H|Acc], R).
 
-% 5) my_nth
+% 5) my_nth/3
 my_nth([], _, []).
 my_nth(L, 1, L).
 my_nth([_|T], N, R) :- 
   N1 is N - 1,
   my_nth(T, N1, R).
 
-% 6) my_remove
+% 6) my_remove/3
 my_remove(_, [], []).
 my_remove(X, [H|T], R) :-
   ( H == X
@@ -32,7 +32,7 @@ my_remove(X, [H|T], R) :-
   ; R = [H|R1], my_remove(X, T, R1)
   ).
 
-% 7) my_subst
+% 7) my_subst/4
 my_subst(_, _, [], []).
 my_subst(X, Y, [H|T], R) :-
   ( X == H
@@ -49,7 +49,7 @@ my_subst(X, Y, [H|T], [H|R]) :-
     my_subst(X, Y, T, R).
 */
 
-% 8) my_subset
+% 8) my_subset/3
 my_subset(_, [], []).
 my_subset(P, [H|T], [H|R]) :-
   call(P, H), !,
@@ -57,7 +57,7 @@ my_subset(P, [H|T], [H|R]) :-
 my_subset(P, [_|T], R) :-
   my_subset(P, T, R).
 
-& 9) my_add
+% 9) my_add/3
 my_add(N1, N2, R) :- 
   add_digits(N1, N2, 0, R1), 
   fix_zero(R1, R).
@@ -83,7 +83,7 @@ add_digits([A|TA], [B|TB], C, [D|TR]) :-
 fix_zero([], [0]).
 fix_zero(L, L).
 
-% 10) my_merge
+% 10) my_merge/3
 my_merge([], L2, L2).
 my_merge(L1, [], L1).
 my_merge([H1|T1], [H2|T2], [H1|R]) :-
@@ -92,7 +92,7 @@ my_merge([H1|T1], [H2|T2], [H1|R]) :-
 my_merge([H1|T1], [H2|T2], [H2|R]) :-
   my_merge([H1|T1], T2, R).
 
-% 11) my_sublist
+% 11) my_sublist/2
 my_sublist([], _) :- !.
 my_sublist(Sub, L) :-
   starts_with(Sub, L), !.
@@ -102,12 +102,12 @@ my_sublist(Sub, [_|T]) :-
 starts_with([], _).
 starts_with([H|TP], [H|TL]) :- starts_with(TP, TL).
 
-% 12) my_assoc
+% 12) my_assoc/3
 my_assoc(_, [], _) :- fail.
 my_assoc(A, [A, H|_], H) :- !.
 my_assoc(A, [_B, _C|T], R) :- my_assoc(A, T, R).
 
-% 13) my_replace
+% 13) my_replace/3
 my_replace(_, [], []).
 my_replace(L, [H|T], [R1|R]) :-
   my_assoc(H, L, R1), !,
